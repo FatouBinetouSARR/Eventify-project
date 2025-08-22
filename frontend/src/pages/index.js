9720b4d5f697e5e237dcaf0545f24668bc446557
galsencode12 <mohamedcoulibaly3129@gmail.com>
2025-08-22 11:39:51 +0000

    Create index.js

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,5 @@
+// Export des pages
+export { default as Home } from './Home';
+export { default as Login } from './Login';
+export { default as Register } from './Register';
+export { default as EventDetail } from './EventDetail';
