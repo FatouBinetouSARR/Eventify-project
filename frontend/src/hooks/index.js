743774f2772c607a3dc10cf75c225143556ad841
galsencode12 <mohamedcoulibaly3129@gmail.com>
2025-08-22 11:39:28 +0000

    Create index.js

---
+++ frontend/src/hooks/index.js
@@ -0,0 +1,13 @@
+// Export des hooks personnalisés
+// Ce fichier sera utilisé pour centraliser l'export de tous les hooks
+
+// Exemple d'export (à décommenter quand les hooks seront créés) :
+// export { default as useAuth } from './useAuth';
+// export { default as useEvents } from './useEvents';
+// export { default as useLocalStorage } from './useLocalStorage';
+// export { default as useForm } from './useForm';
+// export { default as useApi } from './useApi';
+// export { default as useDebounce } from './useDebounce';
+
+// Pour l'instant, export vide
+export {};
